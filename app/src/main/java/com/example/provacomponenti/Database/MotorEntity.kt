7ce1823843b204a774a4b60470096fcde9fa9a5c
LoryBug <72506393+LoryBug@users.noreply.github.com>
2022-05-27 10:35:07 +0200

    add url moto

--- app/src/main/java/com/example/provacomponenti/Database/MotorEntity.kt
+++ app/src/main/java/com/example/provacomponenti/Database/MotorEntity.kt
@@ -20,13 +20,13 @@ data class Motor(
 
 var motors = listOf(
     Motor(
-        1,"Honda", "Dominator","650cc","Dual",40,160,290522,301022,"caio"
+        1,"Honda", "Dominator","650cc","Dual",40,160,290522,301022,"https://cdn.inmoto.it/images/2021/02/19/181747538-f1c92825-4e21-4c33-bc17-a86c928c8988.jpg"
     ),
     Motor(
-        2,"Yamaha", "Teneré","700cc","Dual",72,202,290522,301022,"caio"
+        2,"Yamaha", "Teneré","700cc","Dual",72,202,290522,301022,"https://h2v5h9c2.rocketcdn.me/wp-content/uploads/2022/03/Yamaha-Te%CC%81ne%CC%81re%CC%81-700-2022-34-1200x675-2.jpeg"
     ),
     Motor(
-        3," Ducati", "V4s","1100cc","Carenata",203,175,290522,301022,"caio"
+        3," Ducati", "V4s","1100cc","Carenata",203,175,290522,301022,"https://i.ytimg.com/vi/oH1-OJe8pMI/maxresdefault.jpg"
     ),
 
 )
